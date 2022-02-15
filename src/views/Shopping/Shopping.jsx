20504f9e15e2397a53bf33441d26511cef38c48e
Violet Stewart <vkazoo2@gmail.com>
2022-02-15 14:39:51 -0800

    items reducer function and initial state set

--- src/views/Shopping/Shopping.jsx
+++ src/views/Shopping/Shopping.jsx
@@ -1,4 +1,40 @@
 import React from 'react';
+import { useState, useReducer } from 'react';
+
+const initialItems = [
+  { id: 0, text: 'Cherries 🍒', done: false },
+  { id: 1, text: 'Peaches 🍑', done: false },
+  { id: 2, text: 'Apples 🍎 ', done: false },
+];
+
+function itemsReducter(items, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...items,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'changed': {
+      return items.map((item) => {
+        if (item.id === action.task.id) {
+          return action.task;
+        }
+        return item;
+      });
+    }
+    case 'deleted': {
+      return items.filter((item) => item.id !== action.id);
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`);
+    }
+  }
+}
 
 export default function Shopping() {
   return <div>Shopping</div>;
