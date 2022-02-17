f8785d06388cfe2bc7f7f6158cb86f54450d5ce6
Violet Stewart <vkazoo2@gmail.com>
2022-02-17 14:29:57 -0800

    add test passes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test.skip('renders learn react link', () => {
+test('renders a shopping list that can add items', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const addInput = screen.getByPlaceholderText('add item');
+  const addButton = screen.getByText('Add Item');
+
+  screen.getByText('Cherries 🍒');
+  screen.getByText('Peaches 🍑');
+  screen.getByText('Apples 🍎');
+
+  expect(screen.queryByText('Meat 🍖')).not.toBeInTheDocument();
+
+  userEvent.type(addInput, 'Meat 🍖');
+  userEvent.click(addButton);
+
+  screen.getByText('Meat 🍖');
 });
