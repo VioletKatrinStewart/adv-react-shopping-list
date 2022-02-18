1fdf94eced1602c9e30ede3de69d089ca4dd5118
Violet Stewart <vkazoo2@gmail.com>
2022-02-17 16:39:48 -0800

    FINALLY GOT CONTEXT WORKING

---
+++ src/context/ListContext.jsx
@@ -0,0 +1,85 @@
+import { createContext, useContext, useEffect, useReducer } from 'react';
+
+const initialItems = [
+  { id: 0, text: 'Cherries 🍒', done: false },
+  { id: 1, text: 'Peaches 🍑', done: false },
+  { id: 2, text: 'Apples 🍎', done: false },
+];
+
+function itemsReducer(items, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...items,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'changed': {
+      return items.map((item) => {
+        if (item.id === action.task.id) {
+          return action.task;
+        }
+        return item;
+      });
+    }
+    case 'deleted': {
+      return items.filter((item) => item.id !== action.id);
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`);
+    }
+  }
+}
+
+export const ListContext = createContext();
+
+const ListProvider = ({ children }) => {
+  const [items, dispatch] = useReducer(itemsReducer, initialItems);
+
+  useEffect(() => {
+    const fetchItems = async () => {};
+    fetchItems();
+  }, []);
+  const handleAddItem = (text) => {
+    dispatch({
+      type: 'added',
+      id: items.length,
+      text,
+    });
+  };
+
+  const handleChangeItem = (task) => {
+    dispatch({
+      type: 'changed',
+      task,
+    });
+  };
+
+  const handleDeleteItem = (taskId) => {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
+    });
+  };
+
+  return (
+    <ListContext.Provider value={{ items, handleAddItem, handleChangeItem, handleDeleteItem }}>
+      {children}
+    </ListContext.Provider>
+  );
+};
+
+export default ListProvider;
+
+export const useList = () => {
+  const context = useContext(ListContext);
+
+  if (!context)
+    throw new Error('You must wrap your component with a ListProvider in order to call useList');
+
+  return context;
+};
--- src/views/Shopping/Shopping.jsx
+++ src/views/Shopping/Shopping.jsx
@@ -1,72 +1,16 @@
 import React from 'react';
-import { useReducer } from 'react';
+import { useList } from '../../context/ListContext';
 import ItemList from '../../components/ItemList/ItemList';
 import AddItem from '../../components/AddItem/AddItem';
 
-const initialItems = [
-  { id: 0, text: 'Cherries 🍒', done: false },
-  { id: 1, text: 'Peaches 🍑', done: false },
-  { id: 2, text: 'Apples 🍎 ', done: false },
-];
-
-function itemsReducer(items, action) {
-  switch (action.type) {
-    case 'added': {
-      return [
-        ...items,
-        {
-          id: action.id,
-          text: action.text,
-          done: false,
-        },
-      ];
-    }
-    case 'changed': {
-      return items.map((item) => {
-        if (item.id === action.task.id) {
-          return action.task;
-        }
-        return item;
-      });
-    }
-    case 'deleted': {
-      return items.filter((item) => item.id !== action.id);
-    }
-    default: {
-      throw Error(`Unknown action: ${action.type}`);
-    }
-  }
-}
-
 export default function Shopping() {
-  const [items, dispatch] = useReducer(itemsReducer, initialItems);
+  const { items, handleAddItem, handleChangeItem, handleDeleteItem } = useList();
 
-  const handleAddItem = (text) => {
-    dispatch({
-      type: 'added',
-      id: items.length + 1,
-      text,
-    });
-  };
-
-  const handleChangeItem = (task) => {
-    dispatch({
-      type: 'changed',
-      task,
-    });
-  };
-
-  const handleDeleteItem = (taskId) => {
-    dispatch({
-      type: 'deleted',
-      id: taskId,
-    });
-  };
   return (
     <>
-      <h1>Shopping List</h1>
-      <ItemList items={items} onChangeItem={handleChangeItem} onDeleteItem={handleDeleteItem} />
+      <h1>Shopping List!</h1>
       <AddItem onAddItem={handleAddItem} />
+      <ItemList items={items} onChangeItem={handleChangeItem} onDeleteItem={handleDeleteItem} />
     </>
   );
 }
