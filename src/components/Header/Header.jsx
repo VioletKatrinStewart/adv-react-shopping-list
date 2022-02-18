e94e689b86675593ac19aba8c4051d35b63d2b43
Violet Stewart <vkazoo2@gmail.com>
2022-02-18 11:00:08 -0800

    got header to display number of items in list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useList } from '../../context/ListContext';
 
 export default function Header() {
-  return <div>Your Groceries</div>;
+  const { items } = useList();
+  return (
+    <div>
+      <h2>Your Groceries</h2>
+      <span>{items.length}</span>
+    </div>
+  );
 }
